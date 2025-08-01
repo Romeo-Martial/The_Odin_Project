5efa9619d0e9e04710dd6e0be5c01e2335bfb20a
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2025-08-01 16:12:09 +0200

    Add the function getHumanChoice to script.js
    This function will prompt the user for an integer between 0 and 2
    and return rock if 0, paper if 1, or scissors if 2.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,9 @@
 // Get the computer's choice
 function getComputerChoice() {
+
     // get a random number between 0 and 2
     const randomNumber = Math.floor(Math.random() * 3);
+
     // if the random number is 0, return "rock"
     // if the random number is 1, return "paper"
     // if the random number is 2, return "scissors"
@@ -14,3 +16,26 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+// Get the human choice
+function getHumanChoice(){
+
+    // Prompt the user for an integer between 0 and 2
+    const userChoice = parseInt(prompt(`Please enter your choice:
+        - 0 for rock
+        - 1 for paper
+        - 2 for scissors: `));
+    
+    // If O return "rock"
+    // If 1 return "paper"
+    // If 2 return "scissors"
+    switch(userChoice){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
